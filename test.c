929379fe0063f17dcd5dc35e27919956e8b9ac7f
rutujar <rutujarajesh23@gmail.com>
2018-08-06 12:45:01 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
- 
 int main()
 {
-  int c, n=20, fact = 1;
- 
-
- 
-  for (c = 1; c <= n; c++)
-    fact = fact * c;
- 
-  printf("Factorial of %d = %d\n", n, fact);
- 
-  return 0;
+ char a[10];
+ a[10] = 0;
+ return 0;
 }
+
